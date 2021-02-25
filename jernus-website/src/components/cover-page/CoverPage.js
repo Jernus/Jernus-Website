ea60c57e8dce1725fcb72c065a223664a28d3f3b
Jernus <jerry7626@yahoo.com.tw>
2021-02-25 16:38:11 +1300

    20210225 finish cover page

--- jernus-website/src/components/cover-page/CoverPage.js
+++ jernus-website/src/components/cover-page/CoverPage.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 import Navbar from '../navbar/Navbar';
 import './CoverPage.css';
+import Typing from 'react-typing-animation';
 
 class CoverPage extends Component{
     render(){
         return(
             <div id="cover-page">
                 <Navbar/>
+                <div id="cover-body">
+                    <div id="cover-content">
+                        <div>
+                            <p id="cover-head">Hi! I'm Jerry</p>
+                            <Typing loop={true} speed={70} hideCursor={true}>
+                                <p className="cover-word">Master of Information Technology</p>
+                                <Typing.Backspace count={35} delay={500} />
+                                <p className="cover-word">Love Web Development</p>
+                                <Typing.Backspace count={25} delay={500} />
+                                <p className="cover-word">Fan of Djokovic</p>
+                                <Typing.Backspace count={20} delay={500} />
+                            </Typing>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
